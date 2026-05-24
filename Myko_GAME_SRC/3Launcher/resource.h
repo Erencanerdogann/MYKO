7c692b54cf467079daa0a0c907a622996befd86a
Erencanerdogann <99616505+Erencanerdogann@users.noreply.github.com>
2026-05-24 23:58:43 +0300

    snapshot: 3 GIF dinamik entegrasyon oncesi

--- Myko_GAME_SRC/3Launcher/resource.h
+++ Myko_GAME_SRC/3Launcher/resource.h
@@ -8,12 +8,14 @@
 #define IDB_BITMAP1                     104
 #define IDB_LOADING                     105
 #define IDR_TBL_HASHES                  106
+#define IDB_LOADING_SAFE                107
+#define IDB_LOADING_ERROR               108
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_RESOURCE_VALUE        109
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
